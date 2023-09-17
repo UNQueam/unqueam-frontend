92bb89366e2450252b7133335c2179955aa80423
Nicolas De Maio <nicolas.demaio19@gmail.com>
2023-09-17 12:00:03 -0300

    Edit fonts sizes

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,7 +1,7 @@
 <script setup>
 import {ref} from 'vue';
 
-const usernameOrEmail = ref('');
+const username = ref('');
 const password = ref('');
 
 </script>
@@ -9,20 +9,20 @@ const password = ref('');
 <template>
     <div class="flex flex-column align-items-center justify-content-center mt-8 col-11 m-auto">
       <div style="border-radius: 56px; padding: 0.3rem; background: linear-gradient(180deg, var(--primary-color) 10%, rgba(33, 150, 243, 0) 30%)">
-        <div class="w-full surface-card py-8 px-5 sm:px-8" style="border-radius: 53px">
+        <div class="w-full surface-card py-7 px-5 sm:px-8" style="border-radius: 53px">
           <div class="text-center mb-6">
-            <div class="text-900 text-3xl font-medium mb-3">¡Bienvenido!</div>
+            <div class="text-900 text-2xl font-medium mb-3">¡Bienvenido!</div>
             <span class="text-600 font-medium">Inicia sesion para continuar</span>
           </div>
 
           <div>
-            <label class="block text-900 text-xl font-medium mb-2" for="email1">Usuario o correo electronico</label>
-            <InputText id="email1" v-model="usernameOrEmail" class="w-full md:w-30rem mb-5" placeholder="Usuario o correo electronico" style="padding: 1rem" type="text" />
+            <label class="block text-900 font-medium mb-2" for="email1">Nombre de usuario</label>
+            <InputText id="email1" v-model="username" class="w-full md:w-30rem mb-5" placeholder="Tú nombre de usuario" style="padding: 1rem" type="text" />
 
-            <label class="block text-900 font-medium text-xl mb-2" for="password1">Contraseña</label>
+            <label class="block text-900 font-medium mb-2" for="password1">Contraseña</label>
             <Password id="password1" v-model="password" :feedback="false" :inputStyle="{ padding: '1rem' }" :toggleMask="true" class="w-full mb-3" inputClass="w-full" placeholder="Contraseña"></Password>
 
-            <Button class="w-full p-3 mt-5 text-xl" label="Ingresar"></Button>
+            <Button class="w-full p-3 mt-5 " label="Ingresar"></Button>
           </div>
         </div>
       </div>
