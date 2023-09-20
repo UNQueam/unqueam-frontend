f1732ee8fd4220dc1614f770532102a0a851083f
Nicolas De Maio <nicolas.demaio19@gmail.com>
2023-09-19 22:59:11 -0300

    Fix responsive register

--- src/service/AuthenticationService.ts
+++ src/service/AuthenticationService.ts
@@ -1,5 +1,12 @@
 import axios, {AxiosResponse} from 'axios';
 
+interface UserSignUp {
+    email: string;
+    username: string;
+    password: string;
+    confirmedPassword: string;
+}
+
 class AuthenticationService {
 
     async login(username: string, password: string): Promise<AxiosResponse> {
@@ -7,6 +14,21 @@ class AuthenticationService {
             return await axios.post(`http://localhost:8080/api/auth/signIn`, {
                 username,
                 password,
+            });
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async register(user: UserSignUp): Promise<AxiosResponse> {
+        try {
+            let username = user.username
+            let email = user.email
+            let password = user.password
+            return await axios.post(`http://localhost:8080/api/auth/signUp`, {
+                username,
+                email,
+                password
             });
         } catch (error) {
             throw error;
--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -4,12 +4,14 @@ import {ref} from 'vue';
 import {useVuelidate} from '@vuelidate/core';
 import {email, helpers, required} from '@vuelidate/validators';
 import {getCustomError} from '@/utils/FormErrorMessageHandler';
+import AuthenticationService from "@/service/AuthenticationService";
+import {useRouter} from "vue-router";
+import {useAuthStore} from "@/stores/authStore";
 
 
-const passwordRegexVal = (value) =>  {
+const passwordRegexVal = (value) => {
   return /^(?=.*[A-Z]).{8,}$/.test(value);
 }
-
 
 //Vuelidate:
 
@@ -21,145 +23,159 @@ const user = ref({
 });
 
 const rules = {
-  email: { required, email }, 
-  username: { required },
-  password: { 
+  email: {required, email},
+  username: {required},
+  password: {
     required,
-    passwordRegexVal: helpers.withMessage("La contraseña debe contener al menos 8 caracteres y 1 Mayuscula", passwordRegexVal)
-   },
+    passwordRegexVal: helpers.withMessage("La contraseña debe tener al menos 8 caracteres y 1 Mayuscula", passwordRegexVal)
+  },
   confirmedPassword: {
     required,
     sameAsPassword: (value) => value === user.value.password,
   },
 };
 
-
-
 const $externalResults = ref({})
 
 const submitted = ref(false);
 
-//REEMPLAZAR ESTE DICCIONARIO CON LOS ERRORES PROVENIENTES DEL BACKEND.
-const a = {
-  "email": ["email repetido", "a"], 
-}
+const v$ = useVuelidate(rules, user, {$externalResults});
 
-$externalResults.value = a;
+const isProcessingRequest = ref(false)
 
-const v$ = useVuelidate(rules, user, { $externalResults });
+const authService = new AuthenticationService();
+const router = useRouter();
+const authStore = useAuthStore();
 
-const submitForm = () => {
+async function submitForm() {
+  isProcessingRequest.value = true
+  $externalResults.value = {}
   submitted.value = true;
+
   v$.value.$validate();
-  console.log(v$.value.$error);
+
   if (!v$.value.$error) {
-        // if ANY fail validation
-        alert('Form successfully submitted.')
-      } else {
-        alert('Form failed validation')
-      }
-  console.log(v$);
-    if (v$.value.$invalid) { //ASI SABES SI ES INVALIDO.
-      return console.log("invalid");
+    try {
+      const response = await authService.register(user.value)
+      authStore.saveAuthenticationInfo(response.data);
+      router.push('/');
+    } catch (error) {
+      console.log(error.response.data)
+      $externalResults.value = error.response.data.errors
+    } finally {
+      isProcessingRequest.value = false
     }
+  }
+  isProcessingRequest.value = false
 }
-
-
-
 </script>
 
 <template>
-    <div class="flex flex-column align-items-center justify-content-center mt-8 col-11 m-auto">
-      <div style="border-radius: 56px; padding: 0.3rem; background: linear-gradient(180deg, var(--primary-color) 10%, rgba(33, 150, 243, 0) 30%)">
-        <div class="w-full surface-card py-7 px-5 sm:px-8" style="border-radius: 53px">
-          <div class="text-center mb-6">
-            <div class="text-900 text-2xl font-medium mb-3">Registro</div>
-            <span class="text-600 font-medium">Crea tu cuenta para continuar</span>
-          </div>
+  <div class="flex flex-column align-items-center justify-content-center mt-8 col-11 m-auto">
+    <div
+        style="border-radius: 56px; padding: 0.3rem; background: linear-gradient(180deg, var(--primary-color) 10%, rgba(33, 150, 243, 0) 30%)">
+      <div class="w-full surface-card py-7 sm:px-8 px-5 " style="border-radius: 53px; min-width: 300px;">
+        <div class="text-center mb-6">
+          <div class="text-900 text-2xl font-medium mb-3">Registro</div>
+          <span class="text-600 font-medium">Crea tu cuenta para continuar</span>
+        </div>
+        <div>
+          <form @submit.prevent="submitForm">
+            <label class="block text-900 font-medium mb-2" for="email1">Email <span class="required">*</span></label>
+            <InputText
+                id="email1"
+                v-model.trim="user.email"
+                :class="{ 'p-invalid': submitted && v$.email.$error }"
+                autofocus
+                class="w-full" placeholder="Ingresa tu correo electronico"
+                style="padding: 1rem"
+                type="text"
+            />
 
-          <div>
-            <div class="field p-fluid mb-5">
-                <label class="block text-900 font-medium mb-2" for="email1">Email <span class="required">*</span></label>
-                <InputText 
-                    id="email1" 
-                    v-model.trim="user.email" 
-                    class="w-full" 
-                    placeholder="Ingresa tu correo electronico" 
-                    style="padding: 1rem" type="text" 
-                    autofocus
-                    :class="{ 'p-invalid': submitted && v$.email.$error }"
-                />
-                
-                <small v-for="error of v$.email.$errors" :key="error.$uid" class="p-error">{{ getCustomError("email",error.$validator, error) + ". " }} <br> </small>
-            </div>
-            <div class="md:w-30rem">
-            </div>
-            
-            <div class="field p-fluid mb-5">
-                <label class="block text-900 font-medium mb-2" for="username">Usuario <span class="required">*</span></label>
-                <InputText 
-                    id="username" 
-                    v-model.trim="user.username" 
-                    class="w-full" 
-                    placeholder="Ingresa tu nombre de usuario" 
-                    style="padding: 1rem" type="text" 
-                    autofocus
-                    :class="{ 'p-invalid': submitted && v$.username.$error }"
-                />
-                <small  v-for="error of v$.username.$errors" :key="error.$uid" class="p-error">{{ getCustomError("username",error.$validator,error) + ". " }} <br> </small>
-
+            <div class="error-container">
+              <small v-for="error of v$.email.$errors" :key="error.$uid" class="p-error">{{ getCustomError("email", error.$validator, error) + ". " }}</small>
             </div>
 
-            <div class="field p-fluid mb-5">
-                <label class="block text-900 font-medium mb-2" for="password1">Contraseña <span class="required">*</span></label>
-                <Password 
-                    id="password1" 
-                    v-model="user.password" 
-                    :feedback="false" 
-                    :inputStyle="{ padding: '1rem' }" 
-                    :toggleMask="true"
-                    :class="{ 'p-invalid': submitted && v$.password.$error }"
-                    class="w-full" 
-                    inputClass="w-full"
-                    autofocus 
-                    placeholder="Al menos 8 caracteres y 1 mayus"
-                    >
-                </Password>
-                <small  v-for="error of v$.password.$errors" :key="error.$uid" class="p-error">{{ getCustomError("password",error.$validator, error) + ". " }} <br> </small>
+            <div class="md:w-30rem"/>
+
+            <label class="block text-900 font-medium mb-2 mt-1" for="username">Usuario <span
+                class="required">*</span></label>
+            <InputText
+                id="username"
+                v-model.trim="user.username"
+                :class="{ 'p-invalid': submitted && v$.username.$error }"
+                autofocus
+                class="w-full" placeholder="Ingresa tu nombre de usuario"
+                style="padding: 1rem"
+                type="text"
+            />
+            <div class="error-container">
+              <small v-for="error of v$.username.$errors" :key="error.$uid" class="p-error">{{ getCustomError("username", error.$validator, error) + ". " }}</small>
             </div>
 
-            <div class="field p-fluid mb-3">
-                <label class="block text-900 font-medium mb-2" for="password2">Confirmar Contraseña <span class="required">*</span></label>
-                <Password 
-                    id="password2" 
-                    v-model="user.confirmedPassword" 
-                    :feedback="false" 
-                    :inputStyle="{ padding: '1rem' }" 
-                    :toggleMask="true"
-                    :class="{ 'p-invalid': submitted && v$.confirmedPassword.$error }" 
-                    class="w-full" 
-                    inputClass="w-full"
-                    autofocus 
-                    placeholder="Al menos 8 caracteres y 1 mayus"
-                    >
-                </Password>
-                  <small  v-for="error of v$.confirmedPassword.$errors" :key="error.$uid" class="p-error">{{ getCustomError("confirmedPassword",error.$validator,error) + ". " }}<br> </small>
+
+            <label class="block text-900 font-medium mb-2 mt-1" for="password1">Contraseña <span
+                class="required">*</span></label>
+            <Password
+                id="password1"
+                v-model="user.password"
+                :class="{ 'p-invalid': submitted && v$.password.$error }"
+                :feedback="false"
+                :inputStyle="{ padding: '1rem' }"
+                :toggleMask="true"
+                autofocus
+                class="w-full"
+                inputClass="w-full"
+                placeholder="Al menos 8 caracteres y 1 mayus"
+            >
+            </Password>
+            <div class="error-container">
+              <small v-for="error of v$.password.$errors" :key="error.$uid" class="p-error">{{ getCustomError("password", error.$validator, error) + ". " }}</small>
             </div>
 
-            <Button class="w-full p-3 mt-5" label="Registrarse" @click="submitForm"></Button>
+            <label class="block text-900 font-medium mb-2 mt-1" for="password2">Confirmar Contraseña <span
+                class="required">*</span></label>
+            <Password
+                id="password2"
+                v-model="user.confirmedPassword"
+                :class="{ 'p-invalid': submitted && v$.confirmedPassword.$error }"
+                :feedback="false"
+                :inputStyle="{ padding: '1rem' }"
+                :toggleMask="true"
+                autofocus
+                class="w-full"
+                inputClass="w-full"
+                placeholder="Al menos 8 caracteres y 1 mayus"
+            >
+            </Password>
+            <div class="error-container">
+              <small v-for="error of v$.confirmedPassword.$errors" :key="error.$uid" class="p-error">{{ getCustomError("confirmedPassword", error.$validator, error) + ". " }}</small>
+            </div>
+
+            <Button :disabled="isProcessingRequest" class="w-full p-3 mt-5" label="Registrarse" type="submit"></Button>
             <div class="text-center w-100 mt-2">
-                <router-link class="font-medium no-underline m-auto cursor-pointer" style="color: var(--primary-color)" to="/login">¿Ya tienes cuenta? Ingresa</router-link>
+              <router-link class="font-medium no-underline m-auto cursor-pointer" style="color: var(--primary-color)"
+                           to="/login">¿Ya tienes cuenta? Ingresa
+              </router-link>
             </div>
-
-          </div>
+          </form>
         </div>
       </div>
     </div>
+  </div>
 </template>
 
 <style scoped>
 
 .required {
   color: #9b2a3a;
+}
+
+.error-container {
+  height: 1.5rem; /* Establece una altura fija para el contenedor de mensajes de error */
+  display: flex; /* Usa Flexbox para alinear verticalmente los mensajes de error */
+  flex-direction: column; /* Alinea los mensajes de error en una columna */
+  justify-content: center; /* Centra verticalmente los mensajes de error */
+  margin-top: 0.5rem;
 }
 </style>
