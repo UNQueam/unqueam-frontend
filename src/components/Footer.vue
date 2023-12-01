3446d827d7187a96ff9914b461198a67594d5c73
TrejoJulian <trejojulian998@gmail.com>
2023-11-30 22:49:44 -0300

    Footer added to the webpage

---
+++ src/components/Footer.vue
@@ -0,0 +1,21 @@
+<script setup>
+</script>
+
+<template>
+  <div class="layout-footer py-3">
+    <img src="/public/Unqueam_logo_image.png" alt="Logo" height="20">
+    <span class="font-medium ml-2 text-500">by Nicolas De Maio - Julian Daniel Trejo</span>
+  </div>
+</template>
+
+<style scoped>
+.layout-footer {
+  transition: margin-left .2s;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-top: 1px solid var(--surface-border);
+  width: 95%;
+  margin: auto;
+}
+</style>
